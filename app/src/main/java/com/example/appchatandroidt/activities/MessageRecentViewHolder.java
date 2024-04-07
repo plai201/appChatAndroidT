7a42bb794b9fd35a7f098f7189369fbd436f964e
plai201 <nguyenthanhtungksa@gmail.com>
2024-04-08 05:35:41 +0700

    update

---
+++ app/src/main/java/com/example/appchatandroidt/activities/MessageRecentViewHolder.java
@@ -0,0 +1,2 @@
+package com.example.appchatandroidt.activities;public class MessageRecentViewHolder {
+}
---
+++ app/src/main/java/com/example/appchatandroidt/models/MyFirebaseMessagingService.java
@@ -0,0 +1,2 @@
+package com.example.appchatandroidt.models;public class MyFirebaseMessagingService {
+}
